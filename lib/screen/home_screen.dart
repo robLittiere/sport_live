72453ec3ff59949faa97f77da84638cf19bc27d9
Paul Geneve <geneve.paul1295@gmail.com>
2022-03-21 11:49:44 +0100

    wip infinite scroll

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -27,7 +27,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Image.asset('images/sport_live.png', width: 72, height: 49, fit: BoxFit.cover),
+          title: Image.asset('assets/sport_live.png', width: 72, height: 49, fit: BoxFit.cover),
           backgroundColor: const Color(0xffe13438),
         ),
         body: PageView(
--- lib/screen/views/home_view.dart
+++ lib/screen/views/home_view.dart
@@ -5,8 +5,12 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-     return const Center(
-       child: Text('Home page View')
-       );
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text('#hashtag'),
+        );
+      },
+    );
   }
 }
