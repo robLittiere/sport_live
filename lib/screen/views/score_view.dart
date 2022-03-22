92d1eb3b6b71abafcd6c3a8c543e478e64722b0d
Xamilu <71848840+Xamilu@users.noreply.github.com>
2022-03-22 10:52:28 +0100

    Create ScoreView

--- lib/screen/views/score_view.dart
+++ lib/screen/views/score_view.dart
@@ -6,8 +6,147 @@ class ScoreView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-     return const Center(
-       child: Text("Liste des scores")
-       );
+     return SingleChildScrollView(
+       child: Column(
+         children: [
+           Container(
+             margin: const EdgeInsets.only(top: 20),
+             child: Center(
+               child: Text(
+                 "Résultats",
+                 textAlign: TextAlign.center,
+                 style: TextStyle(
+                   fontSize: 30,
+                   color: Colors.black87
+                 ),
+               ),
+             ),
+           ),
+           Container(
+             padding: const EdgeInsets.only(left: 30),
+             margin: const EdgeInsets.only(top: 20, right: 120, bottom: 40),
+             width: MediaQuery.of(context).size.width,
+             height: 60,
+             color: Color(0xffe13438),
+             child: Row(
+               children: [
+                 Text("Football",
+                 style: TextStyle(
+                   fontSize: 20,
+                   color: Colors.white,
+                 ),),
+               ],
+             ),
+           ),
+           sportsLabel,
+           sportsLabel,
+           sportsLabel,
+           sportsLabel,
+           sportsLabel,
+           sportsLabel,
+           sportsLabel,
+           sportsLabel,
+           sportsLabel,
+         ],
+       ),
+     );
+
+
   }
 }
+
+Widget sportsLabel = Row(
+  mainAxisSize: MainAxisSize.max,
+  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  children: [
+    Container(
+      padding: const EdgeInsets.only(left: 30),
+      margin: const EdgeInsets.only(bottom: 40),
+      width: 150,
+      height: 50,
+      color: Color(0xffe13438),
+      child: Row(
+        children: [
+          Text("Paris-SG",
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+            ),)
+        ],
+      ),
+    ),
+    Container(
+      margin: const EdgeInsets.only(bottom: 40),
+      width: 100,
+      height: 50,
+      color: Colors.white,
+      child: Center(
+        child: Text(
+          "0-0",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 25,
+            color: Colors.black87,
+          ),
+        ),
+      ),
+    ),
+    Container(
+      padding: const EdgeInsets.only(left: 30),
+      margin: const EdgeInsets.only(bottom: 40),
+      width: 150,
+      height: 50,
+      color: Color(0xffe13438),
+      child: Row(
+        children: [
+          Text("Marseille",
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+            ),)
+        ],
+      ),
+    ),
+  ],
+);
+
+/*
+Column(
+mainAxisSize: MainAxisSize.max,
+children: [
+Text("Résultats",
+style: TextStyle(
+fontSize: 30,
+color: Colors.black87,
+),),
+
+
+Container(
+padding: const EdgeInsets.only(left: 30),
+margin: const EdgeInsets.only(top: 80, right: 120, bottom: 40),
+width: MediaQuery.of(context).size.width,
+height: 60,
+color: Color(0xffe13438),
+child: Row(
+children: [
+Text("Football",
+style: TextStyle(
+fontSize: 20,
+color: Colors.white,
+),)
+],
+),
+),
+sportsLabel,
+sportsLabel,
+sportsLabel,
+sportsLabel,
+sportsLabel,
+sportsLabel,
+sportsLabel,
+sportsLabel,
+sportsLabel,
+sportsLabel
+],
+
+*/
