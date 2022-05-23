50072c8236c3d28eba7222fb049c39646104235d
rob <robinl.95@orange.fr>
2022-05-23 11:49:52 +0200

    adapted time for api request

--- lib/api_manager.dart
+++ lib/api_manager.dart
@@ -1,19 +1,31 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
 
 import 'package:sport_live/models/SoccerModel.dart';
 
 class SoccerApi {
 
-  static const String apiSoccerUrl = "https://v3.football.api-sports.io/fixtures?season=2021&league=61&from=2022-03-17&to=2022-03-24";
+  static var apiSoccerUrl = "https://v3.football.api-sports.io/fixtures?season=2021&league=39";
 
   static const headers = {
     'x-rapidapi-key': "f2cc166b6b3aedeaefbdd22ee6d90412"
   };
 
 
-  Future<List<SoccerMatch>> getAllMatches() async {
+  Future<List> getAllMatches() async {
+    // Get date now
+    DateTime now = DateTime.now();
+    var todayFormated = DateFormat('yyyy-MM-dd').format(now);
+
+    var beforeDate = now.subtract(const Duration(days: 7));
+    var beforeDateFormated = DateFormat('yyyy-MM-dd').format(beforeDate);
+
+    // Add date to string
+    apiSoccerUrl += "&from=" + beforeDateFormated + "&to=" + todayFormated;
+
+
     final res = await http.get(Uri.parse(apiSoccerUrl), headers: headers);
     var body;
 
@@ -21,8 +33,8 @@ class SoccerApi {
       body = jsonDecode(res.body);
       List<dynamic> matchesList = body['response'];
       List<SoccerMatch> matches = matchesList.map((dynamic item) => SoccerMatch.fromJson(item)).toList();
-
-      return matches;
+      var reversedMatches = List.from(matches.reversed);
+      return reversedMatches;
     }
     else {
       throw Exception('Failed to load Matches');
